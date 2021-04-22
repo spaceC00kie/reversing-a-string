0e01e2e23e7fa819d4bfe85369798b94309595b5
spaceC00kie <kirstie317@gmail.com>
2021-04-22 02:55:30 -0400

    steps outlined first completed

---
+++ stringReverse.js
@@ -0,0 +1,16 @@
+const revStr = () => {}
+
+//make string into an array
+str = "gotit"
+const makeArr = str.split("")
+console.log(makeArr)
+
+//define left and right pointer variables
+
+//while loop moving pointers in until the pointers cross
+
+//switch the values at the pointers
+
+//make array into string
+
+//return string
