b31ea5da29888e321c6c9f9f0bf0b09feba6ddd2
spaceC00kie <kirstie317@gmail.com>
2021-04-23 23:57:50 -0400

    progress on alg

--- stringReverse.js
+++ stringReverse.js
@@ -1,16 +1,17 @@
-const revStr = (x) => {
+const revStr = str => {
+  //make string into an array
+  let arr = str.split("")
+  //define left and right pointer variables
+  let left = 0
+  let right = arr.length - 1
+  //while loop moving pointers in until the pointers cross
+  while (left < right) {
+    //switch the values at the pointers
+    //move both pointers inward
+  }
+  //make array into string
 
-//make string into an array
-
-const makeArr = x.split("")
-
-//define left and right pointer variables
-
-//while loop moving pointers in until the pointers cross
-
-//switch the values at the pointers
-
-//make array into string
-
-//return string
+  //return string
 }
+
+revStr("testing")
